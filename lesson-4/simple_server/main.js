7df749221343b98859d6479569cad1e38b4ade06
Sebastian <sebasgrammar@hotmail.com>
2020-05-14 01:03:46 -0500

    lesson 4

--- lesson-4/simple_server/main.js
+++ lesson-4/simple_server/main.js
@@ -3,35 +3,24 @@ const port = 3000,
     httpStatus = require("http-status-codes"),
     app = http.Server((request, response) => {
         console.log("Incoming request received.")
+        console.log(typeof request)
+        console.log(typeof response)
         response.writeHead(httpStatus.OK, {
             "Content-Type": "text/html"
         })
+
+        // So this bit is only going to run when the client sends a request.
 
         let responseMessage = "<h1>Hello!!!</h1>"
         response.write(responseMessage);
         response.end()
         console.log(`Sent a response : ${responseMessage}`)
     });
-    // app = http.Server(function (request, response) {
-    //     console.log("Incoming request received.")
-    //     response.writeHead(httpStatus.OK, {
-    //         "Content-Type": "text/html"
-    //     })
 
-    //     console.log(response)
+console.log(typeof http)
 
-    //     let responseMessage = "<h1>Hello, Universe!</h1>";
-    //     response.write(responseMessage);
-    //     response.end();
-    //     console.log(`Sent a response : ${responseMessage}`);
-    // });
 app.listen(port);
 console.log(`The server has started and is listening on port number:
     ➥ ${port}`);
 
 
-    // app = http.Server((request, response) => {
-
-    // })
-
-// console.log(http)
