8a886c93dd36c2a8ba0c51c52dbcfa2094efd2ee
Sebastian <sebasgrammar@hotmail.com>
2020-05-17 01:24:10 -0500

    finished lesson 6

--- lesson-6/serve_html/logic.js
+++ lesson-6/serve_html/logic.js
@@ -1,7 +1,27 @@
 const port = 3000,
     http = require("http"),
-    httpStatus = require("http-status-codes"),
+    httpStatusCodes = require("http-status-codes"),
+    router = require("./router"),
     fs = require("fs");
+
+const contentTypes = {
+    html: {
+        "Content-Type": "text/html"
+    },
+    plain: {
+        "Content-Type": "text/plain"
+    }
+}
+
+const customReadFile = (file, res) => {
+    fs.readFile(`./${file}`, (errors, data) => {
+        if (errors) {
+            console.log("Error reading the file")
+        }
+        res.end(data)
+    })
+
+};
 
 const sendErrorResponse = res => {
     res.writeHead(httpStatus.NOT_FOUND, {
@@ -10,52 +30,72 @@ const sendErrorResponse = res => {
     res.write("<h1>File Not Found!</h1>");
     res.end();
 };
-http
-    .createServer((req, res) => {
-        let url = req.url;
-        if (url.indexOf(".html") !== -1) {
-            res.writeHead(httpStatus.OK, {
-                "Content-Type": "text/html"
-            });
-            customReadFile(`./views${url}`, res);
-        } else if (url.indexOf(".js") !== -1) {
-            res.writeHead(httpStatus.OK, {
-                "Content-Type": "text/javascript"
-            });
-            customReadFile(`./public/js${url}`, res);
-        } else if (url.indexOf(".css") !== -1) {
-            res.writeHead(httpStatus.OK, {
-                "Content-Type": "text/css"
-            });
-            customReadFile(`./public/css${url}`, res);
-        } else if (url.indexOf(".png") !== -1) {
-            res.writeHead(httpStatus.OK, {
-                "Content-Type": "image/png"
-            });
-            customReadFile(`./public/images${url}`, res);
-        } else {
-            sendErrorResponse(res);
-        }
-    })
-    .listen(3000);
-console.log(`The server is listening on port number: ${port}`);
 
-const customReadFile = (file_path, res) => {
-    if (fs.existsSync(file_path)) {
-        fs.readFile(file_path, (error, data) => {
-            if (error) {
-                console.log(error);
-                sendErrorResponse(res);
-                return;
-            }
-            res.write(data);
-            res.end();
-        });
-    } else {
-        sendErrorResponse(res);
-    }
-};
+router.get("/", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, contentTypes.plain);
+    res.end("INDEX");
+});
 
-const writeHead = () => {
-    
-}
+router.get("/index.html", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, contentTypes.html);
+    customReadFile("views/index.html", res);
+});
+
+router.get("/contact.html", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, contentTypes.html);
+    customReadFile("views/contact.html", res)
+});
+
+router.post("/", (req, res) => {
+    res.writeHead(httpStatusCodes.OK, plainTextContentType);
+    res.end("POSTED");
+});
+
+http.createServer(router.handle).listen(3000);
+console.log(`The server is listening on port number: ➥ ${port}`);
+
+// http
+//     .createServer((req, res) => {
+//         let url = req.url;
+//         if (url.indexOf(".html") !== -1) {
+//             res.writeHead(httpStatus.OK, {
+//                 "Content-Type": "text/html"
+//             });
+//             customReadFile(`./views${url}`, res);
+//         } else if (url.indexOf(".js") !== -1) {
+//             res.writeHead(httpStatus.OK, {
+//                 "Content-Type": "text/javascript"
+//             });
+//             customReadFile(`./public/js${url}`, res);
+//         } else if (url.indexOf(".css") !== -1) {
+//             res.writeHead(httpStatus.OK, {
+//                 "Content-Type": "text/css"
+//             });
+//             customReadFile(`./public/css${url}`, res);
+//         } else if (url.indexOf(".png") !== -1) {
+//             res.writeHead(httpStatus.OK, {
+//                 "Content-Type": "image/png"
+//             });
+//             customReadFile(`./public/images${url}`, res);
+//         } else {
+//             sendErrorResponse(res);
+//         }
+//     })
+//     .listen(3000);
+// console.log(`The server is listening on port number: ${port}`);
+
+// const customReadFile = (file_path, res) => {
+//     if (fs.existsSync(file_path)) {
+//         fs.readFile(file_path, (error, data) => {
+//             if (error) {
+//                 console.log(error);
+//                 sendErrorResponse(res);
+//                 return;
+//             }
+//             res.write(data);
+//             res.end();
+//         });
+//     } else {
+//         sendErrorResponse(res);
+//     }
+// };
